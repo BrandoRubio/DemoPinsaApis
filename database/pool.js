8553d3c4245dfe0ef06baa5a257f26f4b26970e6
BrandoRubio <33293044+BrandoRubio@users.noreply.github.com>
2026-03-19 23:46:50 -0600

    .

--- database/pool.js
+++ database/pool.js
@@ -3,9 +3,9 @@ const { Pool } = require('pg');
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: false,
-    /*ssl: {
+    ssl: {
         rejectUnauthorized: false, // Evitar bloqueos en de conexión
-    },*/
+    }
 });
 
 module.exports = pool;
